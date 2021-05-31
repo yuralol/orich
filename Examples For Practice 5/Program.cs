7dc8e93343402af294ae0254a9e0391604257d54
yuralol <84666118+yuralol@users.noreply.github.com>
2021-05-31 12:23:24 +0300

    practice 5

---
+++ Examples For Practice 5/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examples_For_Practice_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int money = 100;
+            int minPrice = 20;
+
+            Console.Write("Введіть ціну товару:");
+            int price = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("Варіант 1 - && логічне І");
+            if (minPrice <= price && price <= money)
+            { 
+                Console.WriteLine("Придбаємо");
+            }
+            else
+            {
+                Console.WriteLine("Не придбаємо");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Варіант 2 - || логічне АБО");
+
+            if (price < minPrice || price > money)
+            {
+                Console.WriteLine("Не придбаємо");
+            }
+            else
+            {
+                Console.WriteLine("Придбаємо");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Варіант 3 - ! НЕ та || логічне АБО");
+ 
+            if (!(price < minPrice || price > money))
+            {
+                Console.WriteLine("Придбаємо");
+            }
+            else
+            {
+                Console.WriteLine("Не придбаємо");
+            }
+        }
+    }
+}
